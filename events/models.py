1def163ef48570b24cd2abc2094cbf8ce8144865
MatejMecka <matej.plavevski+github@gmail.com>
2018-03-21 18:36:30 +0000

    Add model for event

--- events/models.py
+++ events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from location_field.models.plain import PlainLocationField
 
 # Create your models here.
+
+class Event(models.Model):
+	def __str__(self):
+		return "Event: {} - {} ".format(self.eventName, self.date)
+	eventName = models.CharField("Event Name", max_length=100, blank=False)
+	date = models.DateTimeField("Date")
+	description = models.TextField()
+	place = models.CharField("Location", max_length=255, blank=False)
+	coordinates = PlainLocationField(based_fields=['place'], zoom=7)
+
+
--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+"""
+class siteRunning(TestCase):
+   def setUp(self):
+        self.client = Client()
+
+   def test_homepage(self):
+        homepage = client.get('/')
+        self.assertTrue(homepage.response, 200)
+"""
+# todo
